3219ae3b58226431deb1acc0d22b0455779cb91d
AminEl93 <amin_11_iverson@hotmail.com>
2023-10-02 00:07:48 +0200

    Añadir un spinner loader al buscar los países

--- src/app/countries/pages/by-capital-page/by-capital-page.component.ts
+++ src/app/countries/pages/by-capital-page/by-capital-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Country } from '../../interfaces/country';
 import { CountriesService } from '../../services/countries.service';
 
@@ -7,16 +7,23 @@ import { CountriesService } from '../../services/countries.service';
     templateUrl: './by-capital-page.component.html'
 })
 
-export class ByCapitalPageComponent {
+export class ByCapitalPageComponent implements OnInit {
 
     public countries: Country[] = [];
+    public isLoading: boolean = false;
 
     constructor(private _countriesService: CountriesService) { }
     
     searchByCapital(term: string): void {
+        this.isLoading = true;
         this._countriesService.searchCapital(term)
             .subscribe(countries => {
                 this.countries = countries;
+                this.isLoading = false;
             });
+    }
+
+    ngOnInit(): void {
+        
     }
 }
