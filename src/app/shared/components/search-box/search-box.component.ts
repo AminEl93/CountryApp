e38bfea5d5fbe37588a58111c276be265dc8781d
AminEl93 <amin_11_iverson@hotmail.com>
2023-10-02 13:47:44 +0200

    Debounce: mostrar resultados al dejar de escribir

--- src/app/shared/components/search-box/search-box.component.ts
+++ src/app/shared/components/search-box/search-box.component.ts
@@ -1,16 +1,39 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output, OnInit, OnDestroy } from '@angular/core';
+import { Subject, debounceTime, Subscription } from 'rxjs';
 
 @Component({
-  selector: 'shared-search-box',
-  templateUrl: './search-box.component.html'
+    selector: 'shared-search-box',
+    templateUrl: './search-box.component.html'
 })
 
-export class SearchBoxComponent {
+export class SearchBoxComponent implements OnInit, OnDestroy {
     
-    @Input() public placeholder: string = '';  
+    private debouncer: Subject<string> = new Subject<string>();
+    private debouncerSuscription?: Subscription;
+
+    @Input() public placeholder: string = '';
+    @Input() public initialValue: string = '';
+
     @Output() public onValue = new EventEmitter<string>();
-  
+    @Output() public onDebounce = new EventEmitter<string>();
+
+    ngOnInit(): void {
+        this.debouncerSuscription = this.debouncer
+            .pipe( debounceTime(300) )
+            .subscribe(value => {
+                this.onDebounce.emit(value);
+            });
+    }
+
+    ngOnDestroy(): void {
+        this.debouncerSuscription?.unsubscribe();
+    }
+    
     emitValue(value: string): void {
         this.onValue.emit(value);
+    }
+
+    onKeyPress(searchTerm: string): void {
+        this.debouncer.next(searchTerm);
     }
 }
